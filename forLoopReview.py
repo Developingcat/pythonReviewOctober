e412504b330c76fae7960e2bc96b9df398a8d561
Hannah <148793520+Developingcat@users.noreply.github.com>
2024-10-08 14:41:54 +0000

    forLoopReview.py finished, ifElseReview.py started

---
+++ forLoopReview.py
@@ -0,0 +1,43 @@
+"""
+
+Author: Hannah O'Shea 
+Program: forLoop.py
+
+What to do? : 
+
+Make a for Loop that multiplies by 6 and then divides by 2 
+
+Ask for the input from the user-- a number range! X -> Y! Go girl! 
+And then multiply it by 6 
+And then divide by 2 
+"""
+
+x = int(input("Please put the lowest number of the range!: "))
+
+y = int(input("Please input the highest number of the range!: "))
+
+
+
+for d in range(x, y):
+   result = (d*6)/2
+   print(int((result)))
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ifElseReview.py
@@ -0,0 +1,34 @@
+"""
+Author: Hannah O'Shea
+Program: ifElseReview.py
+
+
+1. Create a script that takes in user input
+    a. It has to check if the number is even or odd (if/else statement?)
+    b. If the number is even, it divides it by 2; otherwise (if odd), it'll just print that the number is odd 
+2. Make a function to check! It HAS to check if it's even or odd! 
+3. Convert to integer, smiley face. 
+"""
+
+num = int(input("Enter in a number!!: "))
+
+result = (num % 2)
+
+# Function making!! HUZZAH! 
+
+def checkEvenOdd(num):
+ if (num % 2)==0(print("The number provided is odd.")):
+   print(result) 
+
+# def checkOddEven(num):
+#   if (num % 2)!=0():
+    
+
+
+
+
+
+
+#  else: 
+#    print("The Number Provided is not Even.")
+
